265a279597c43b0aeccbdd6581402451ee11206d
Christian Steimel <me@C14L.com>
2016-07-27 14:57:29 +0200

    Use locally stored images, sw serve placeholder image for 404

--- app/sw.js
+++ app/sw.js
@@ -1,16 +1,17 @@
 // Manual service worker setup
 
 self.addEventListener('fetch', event => {
-    const args = { headers: { 'Content-Type': 'text/html;charset=UTF-8' } };
-    const response = new Response('<p>Reddmeet app</p>', args);
+    //const defaultArgs = { headers: { 'Content-Type': 'text/html;charset=UTF-8' } };
+    //const defaultResponse = new Response('<p>Reddmeet app</p>', args);
 
     event.respondWith(
         fetch(event.request).then(response => {
-            if (response.status == 404)
-                return new Response("NOT FOUND :(", args)
+            if (response.status == 404) {
+                return fetch('/static/nopic.jpg');
+            }
 
             return response;
         })
-        .catch(err => new Response("No network :("), args)
+        .catch(err => new Response("No network :("))
     );
 });
