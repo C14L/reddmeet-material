bf3381f9e98d279ee952c78dcbdbd69b4a8e3c4e
Christian Steimel <me@C14L.com>
2016-08-19 23:26:12 +0200

    Move search options to config

--- app/src/app.js
+++ app/src/app.js
@@ -115,6 +115,40 @@ window.WS_BASE = "ws://localhost:8000/api/v1/ws";
 			});
 	}]);
 
+	app.value('fProfileOpts', {
+		'fSexOpts': [
+			{ id: 0, label: "everybody", selected: true },
+			{ id: 1, label: "women who like men", selected: false },
+			{ id: 2, label: "women who like women", selected: false },
+			{ id: 3, label: "women who like queer", selected: false },
+			{ id: 4, label: "men who like women", selected: false },
+			{ id: 5, label: "men who like men", selected: false },
+			{ id: 6, label: "men who like queer", selected: false },
+			{ id: 7, label: "queer who like women", selected: false },
+			{ id: 8, label: "queer who like men", selected: false },
+			{ id: 9, label: "queer who like queer", selected: false },
+		],
+		'fDistanceOpts': [
+			{ id: 0, label: "worldwide", selected: true },
+			{ id: 5000, label: "5000 km / 3100 miles", selected: false },
+			{ id: 2000, label: "2000 km / 1250 miles", selected: false },
+			{ id: 1000, label: "1000 km / 620 miles", selected: false },
+			{ id: 700, label: "700 km / 435 miles", selected: false },
+			{ id: 500, label: "500 km / 310 miles", selected: false },
+			{ id: 300, label: "300 km / 186 miles", selected: false },
+			{ id: 200, label: "200 km / 125 miles", selected: false },
+			{ id: 100, label: "100 km / 62 miles", selected: false },
+			{ id: 50, label: "50 km / 31 miles", selected: false },
+			{ id: 20, label: "20 km / 12 miles", selected: false },
+		],
+		'fOrderOpts': [
+			{ id: "-sr_count", label: "best matches", selected: true },
+			{ id: "-accessed", label: "recently active", selected: false },
+			{ id: "-date_joined", label: "newest members", selected: false },
+			{ id: "-views_count", label: "most viewed", selected: false },
+		],
+		'srOpts': [],
+	});
 
 	app.run(['$http', '$log', 'AuthUserFactory', function reddmeetAppRun($http, $log, AuthUserFactory) {
 		$http.defaults.headers.post['X-CSRFToken'] = get_cookie('csrftoken');
--- app/src/services/SearchResultsFactory.js
+++ app/src/services/SearchResultsFactory.js
@@ -2,10 +2,9 @@
     'use strict';
 
     angular.module('reddmeetApp')
-        .factory('SearchResultsFactory', ['$log', '$http', 'AuthUserFactory', SearchResultsFactory])
-        ;
+    .factory('SearchResultsFactory', ['$log', '$http', 'AuthUserFactory', 'fProfileOpts', SearchResultsFactory]);
 
-    function SearchResultsFactory($log, $http, AuthUserFactory) {
+    function SearchResultsFactory($log, $http, AuthUserFactory, fProfileOpts) {
         var searchApiUrl = API_BASE + '/api/v1/search';
         var apiUrl = API_BASE + '/api/v1/results';
 
@@ -15,40 +14,7 @@
         var currentResults = [];
         var _paramTr = { 'fSexOpts': 'f_sex', 'fDistanceOpts': 'f_distance', 
                          'fOrderOpts': 'order_by', 'srOpts': 'sr-fav' };
-        var _searchOpts = {
-            'fSexOpts': [
-                { id: 0, label: "everybody", selected: true },
-                { id: 1, label: "women who like men", selected: false },
-                { id: 2, label: "women who like women", selected: false },
-                { id: 3, label: "women who like queer", selected: false },
-                { id: 4, label: "men who like women", selected: false },
-                { id: 5, label: "men who like men", selected: false },
-                { id: 6, label: "men who like queer", selected: false },
-                { id: 7, label: "queer who like women", selected: false },
-                { id: 8, label: "queer who like men", selected: false },
-                { id: 9, label: "queer who like queer", selected: false },
-            ],
-            'fDistanceOpts': [
-                { id: 0, label: "worldwide", selected: true },
-                { id: 5000, label: "5000 km / 3100 miles", selected: false },
-                { id: 2000, label: "2000 km / 1250 miles", selected: false },
-                { id: 1000, label: "1000 km / 620 miles", selected: false },
-                { id: 700, label: "700 km / 435 miles", selected: false },
-                { id: 500, label: "500 km / 310 miles", selected: false },
-                { id: 300, label: "300 km / 186 miles", selected: false },
-                { id: 200, label: "200 km / 125 miles", selected: false },
-                { id: 100, label: "100 km / 62 miles", selected: false },
-                { id: 50, label: "50 km / 31 miles", selected: false },
-                { id: 20, label: "20 km / 12 miles", selected: false },
-            ],
-            'fOrderOpts': [
-                { id: "-sr_count", label: "best matches", selected: true },
-                { id: "-accessed", label: "recently active", selected: false },
-                { id: "-date_joined", label: "newest members", selected: false },
-                { id: "-views_count", label: "most viewed", selected: false },
-            ],
-        	'srOpts': [],
-        };
+        var _searchOpts = fProfileOpts;
 
         return { 
                        
